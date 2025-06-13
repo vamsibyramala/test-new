323b30e538157d7d1f0be0b4ae1130a4b9cdca49
vamsi <vamsi@mail.com>
2025-06-13 21:16:16 +0530

    photos updated

--- photos.java
+++ photos.java
@@ -1,3 +1,4 @@
 photos should be shared with in the followers
-photos can have location
+photos can have location and tagline
 we can tag people in photos
+we can make the photo public when ever we want
